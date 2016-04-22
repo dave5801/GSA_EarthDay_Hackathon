cd200b22036f65aa19b34545844c58375414e72a
stephentuso <tusodev@gmail.com>
2016-04-22 12:26:28 -0400

    Update utils

--- app/src/main/java/gov/epa/uvindex/util/UVIndexUtils.java
+++ app/src/main/java/gov/epa/uvindex/util/UVIndexUtils.java
@@ -1,30 +1,81 @@
 package gov.epa.uvindex.util;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by stephentuso on 4/22/16.
  */
-public class UVIndexUtils implements LocationListener {
+public class UVIndexUtils {
+
+    OkHttpClient client = new OkHttpClient();
+    LocationManager locationManager;
+    Context context;
 
     private Double longitude = null;
     private Double latitude = null;
 
-    public UVIndexUtils(Context context) {
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    public interface UVIndexDownloadListener {
+        void success(int index);
+        void error();
+    }
 
-        //TODO (stephentuso): figure out good interval and add check for permission
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, this);
+    public UVIndexUtils(Context context) {
+        this.context = context;
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public void getCurrentUVIndex(UVIndexDownloadListener listener) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
     }
 
     private String getZipFromLocation(Double latitude, Double longitude) {
@@ -54,24 +105,13 @@ public class UVIndexUtils implements LocationListener {
 
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
+    private JSONObject downloadJSON(String url) throws IOException, JSONException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return new JSONObject(response.body().string());
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 }
