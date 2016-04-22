554a6a5e707db62c03f118d90b93db671b369045
stephentuso <tusodev@gmail.com>
2016-04-22 12:11:48 -0400

    Update utils

--- app/src/main/java/gov/epa/uvindex/util/UVIndexUtils.java
+++ app/src/main/java/gov/epa/uvindex/util/UVIndexUtils.java
@@ -6,29 +6,60 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by stephentuso on 4/22/16.
  */
 public class UVIndexUtils implements LocationListener {
 
+    OkHttpClient client = new OkHttpClient();
+    LocationManager locationManager;
+    Context context;
+
     private Double longitude = null;
     private Double latitude = null;
 
-    public UVIndexUtils(Context context) {
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    public interface UVIndexDownloadListener {
+        void success(int index);
+        void error();
+    }
 
-        //TODO (stephentuso): figure out good interval and add check for permission
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, this);
+    public UVIndexUtils(Context context) {
+        this.context = context;
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public void getCurrentUVIndex(UVIndexDownloadListener listener) {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
     }
 
     private String getZipFromLocation(Double latitude, Double longitude) {
         return "";
     }
 
-    public void getUVIndexForZip(String zipCode) {
+    private void downloadUVIndexForZip(String zipCode, UVIndexDownloadListener callback) {
 
     }
 
+    private JSONObject downloadJSON(String url) throws IOException, JSONException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return new JSONObject(response.body().string());
+    }
+
+    /*-- LocationListener methods --*/
     @Override
     public void onLocationChanged(Location location) {
         longitude = location.getLongitude();
